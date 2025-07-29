8e410431152c473def322564f588eed808faa0ce
SajaJawad <saiamaher2002@gmail.com>
2025-07-29 11:46:15 +0300

    create admin panel (Header & Navbar)

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
 
-export const App = () => {
+import React from 'react'
+import Navbar from './components/Navbar'
+import Sidebar from './components/Sidebar'
+
+const App = () => {
   return (
-    <div>
-      <h1>ameer</h1>
+    <div className='bg-gray-50 min-h-screen'>
+
+      <>
+        <Navbar />
+        <hr />
+        <div className='flex w-full'>
+          <Sidebar/>
+        </div>
+      </>
+
     </div>
   )
 }
+
+export default App
