780b2b01460319c3a18d6c2fd45ed15c4d6554bb
SajaJawad <saiamaher2002@gmail.com>
2025-07-30 11:15:40 +0300

    create token login

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,5 +1,5 @@
 
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from './components/Navbar'
 import Sidebar from './components/Sidebar'
 import { Route, Routes } from 'react-router-dom'
@@ -8,16 +8,26 @@ import List from './pages/List';
 import Orders from './pages/Orders';
 import Login from './components/Login';
 
+  import { ToastContainer, toast } from 'react-toastify';
+  import 'react-toastify/dist/ReactToastify.css'
+
+
+
+
+  export const backendUrl =import.meta.env.VITE_BACKEND_URL
+
 const App = () => {
 
-  const [token, setToken] = useState('')
-
-
+  const [token, setToken] = useState(localStorage.getItem('token')? localStorage.getItem('token'):'')
+  useEffect(()=>{
+    localStorage.setItem('token' , token)
+  }, [token])
 
 
   return (
     <div className='bg-gray-50 min-h-screen'>
-      {token === "" ? <Login /> :
+      <ToastContainer />
+      {token === "" ? <Login setToken={setToken}/> :
         <>
           <Navbar />
           <hr />
