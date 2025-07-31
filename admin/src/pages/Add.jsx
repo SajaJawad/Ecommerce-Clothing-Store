147c86dfe84cbe7151ed327977b5d3beaf70c34d
SajaJawad <saiamaher2002@gmail.com>
2025-07-31 10:28:38 +0300

    Create Add Page

--- admin/src/pages/Add.jsx
+++ admin/src/pages/Add.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import { useState } from 'react';
 import { assets } from './../assets/assets';
 
 const Add = () => {
+
+  const [image1 , setImage1] = useState(false)
+  const [image2 , setImage2] = useState(false)
+  const [image3 , setImage3] = useState(false)
+  const [image4 , setImage4] = useState(false)
+
+
+
   return (
     <form className='flex flex-col w-full items-start gap-3'>
       <div>
+
         <p className='mb-2'>Upload Image</p>
         <div className='flex gap-2'>
           <label htmlFor='image1'>
@@ -26,6 +35,9 @@ const Add = () => {
         </div>
       </div>
 
+
+
+
       <div className='w-full'>
         <p className='mb-2'>Product name</p>
         <input className='w-full max-w-[500px] px-3 py-2 ' type="text" placeholder='Type here' required />
@@ -38,6 +50,8 @@ const Add = () => {
       </div>
 
       <div className='flex flex-col sm:flex-row gap-2 w-full sm:gap-8'>
+
+
         <div>
           <p className='mb-2'>Product category</p>
           <select className='w-full px-3 py-2'>
@@ -46,8 +60,8 @@ const Add = () => {
             <option value="Kids">Kids</option>
           </select>
         </div>
-      </div>
-      <div>
+
+
         <div>
           <p className='mb-2'>Sub category</p>
           <select className='w-full px-3 py-2'>
@@ -57,13 +71,44 @@ const Add = () => {
           </select>
         </div>
         <div>
-          <p>Product Price</p>
-          <input type="Number" placeholder='25'/>
+          <p className='mb-2'>Product Price</p>
+          <input className='w-full px-3 py-2  sm:w-[120px]' type="Number" placeholder='25' />
+        </div>
+
+      </div>
+
+      <div>
+        <p className='mb-2'>Product Sizes</p>
+        <div className="flex gap-3">
+          <div >
+            <p className='bg-slate-200 px-3 py-1 cursor-pointer'>S</p>
+          </div>
+
+          <div>
+            <p className='bg-slate-200 px-3 py-1 cursor-pointer'>M</p>
+          </div>
+
+          <div>
+            <p className='bg-slate-200 px-3 py-1 cursor-pointer'>L</p>
+          </div>
+
+          <div>
+            <p className='bg-slate-200 px-3 py-1 cursor-pointer'>XL</p>
+          </div>
+
+          <div>
+            <p className='bg-slate-200 px-3 py-1 cursor-pointer'>XXL</p>
+          </div>
+
         </div>
       </div>
 
+      <div className='flex gap-2 mt-2'>
+        <input type="checkbox" id='bestseller' />
+        <label className='cursor-pointer' htmlFor="bestseller">Add to bestseller</label>
+      </div>
 
-
+      <button className='w-20 py-3 mt-4 bg-black text-white' type='submit'>ADD</button>
     </form>
 
   )
