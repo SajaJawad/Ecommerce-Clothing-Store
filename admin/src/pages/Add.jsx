44b7c440f99a26501f24150cefd46f9f68f85b0a
SajaJawad <saiamaher2002@gmail.com>
2025-07-31 11:28:07 +0300

    Size processing

--- admin/src/pages/Add.jsx
+++ admin/src/pages/Add.jsx
@@ -47,13 +47,13 @@ const Add = () => {
 
       <div className='w-full'>
         <p className='mb-2'>Product name</p>
-        <input className='w-full max-w-[500px] px-3 py-2 ' type="text" placeholder='Type here' required />
+        <input onChange={(e) => setName(e.target.value)} value={name} className='w-full max-w-[500px] px-3 py-2 ' type="text" placeholder='Type here' required />
       </div>
 
 
       <div className='w-full'>
         <p className='mb-2'>Product description</p>
-        <textarea className='w-full max-w-[500px] px-3 py-2 ' type="text" placeholder='Write content here' required />
+        <textarea onChange={(e) => setDescription(e.target.value)} value={description} className='w-full max-w-[500px] px-3 py-2 ' type="text" placeholder='Write content here' required />
       </div>
 
       <div className='flex flex-col sm:flex-row gap-2 w-full sm:gap-8'>
@@ -61,7 +61,7 @@ const Add = () => {
 
         <div>
           <p className='mb-2'>Product category</p>
-          <select className='w-full px-3 py-2'>
+          <select onChange={(e) => setCategory(e.target.value)} value={category} className='w-full px-3 py-2'>
             <option value="Men">Men</option>
             <option value="Women">Women</option>
             <option value="Kids">Kids</option>
@@ -71,7 +71,7 @@ const Add = () => {
 
         <div>
           <p className='mb-2'>Sub category</p>
-          <select className='w-full px-3 py-2'>
+          <select onChange={(e) => setSubCategory(e.target.value)} value={subCategory} className='w-full px-3 py-2'>
             <option value="Topwear">Topwear</option>
             <option value="Bottomwear">Bottomwear</option>
             <option value="Winterwear">Winterwear</option>
@@ -79,7 +79,7 @@ const Add = () => {
         </div>
         <div>
           <p className='mb-2'>Product Price</p>
-          <input className='w-full px-3 py-2  sm:w-[120px]' type="Number" placeholder='25' />
+          <input onChange={(e) => setPrice(e.target.value)} value={price} className='w-full px-3 py-2  sm:w-[120px]' type="Number" placeholder='25' />
         </div>
 
       </div>
@@ -87,24 +87,24 @@ const Add = () => {
       <div>
         <p className='mb-2'>Product Sizes</p>
         <div className="flex gap-3">
-          <div >
-            <p className='bg-slate-200 px-3 py-1 cursor-pointer'>S</p>
+          <div onClick={() => setSizes(prev => prev.includes("S") ? prev.filter(item => item !== "S") : [...prev, "S"])}>
+            <p className={` ${sizes.includes("S") ? "bg-pink-100 " : "bg-slate-200"} px-3 py-1 cursor-pointer`}>S</p>
           </div>
 
-          <div>
-            <p className='bg-slate-200 px-3 py-1 cursor-pointer'>M</p>
+          <div onClick={() => setSizes(prev => prev.includes("M") ? prev.filter(item => item !== "M") : [...prev, "M"])}>
+            <p className={` ${sizes.includes("M") ? "bg-pink-100 " : "bg-slate-200"} px-3 py-1 cursor-pointer`}>M</p>
           </div>
 
-          <div>
-            <p className='bg-slate-200 px-3 py-1 cursor-pointer'>L</p>
+          <div onClick={() => setSizes(prev => prev.includes("L") ? prev.filter(item => item !== "L") : [...prev, "L"])}>
+            <p className={` ${sizes.includes("L") ? "bg-pink-100 " : "bg-slate-200"} px-3 py-1 cursor-pointer`}>L</p>
           </div>
 
-          <div>
-            <p className='bg-slate-200 px-3 py-1 cursor-pointer'>XL</p>
+          <div onClick={() => setSizes(prev => prev.includes("XL") ? prev.filter(item => item !== "XL") : [...prev, "XL"])}>
+            <p className={` ${sizes.includes("XL") ? "bg-pink-100 " : "bg-slate-200"} px-3 py-1 cursor-pointer`}>XL</p>
           </div>
 
-          <div>
-            <p className='bg-slate-200 px-3 py-1 cursor-pointer'>XXL</p>
+          <div onClick={() => setSizes(prev => prev.includes("XXL") ? prev.filter(item => item !== "XXL") : [...prev, "XXL"])}>
+            <p className={` ${sizes.includes("XXL") ? "bg-pink-100 " : "bg-slate-200"} px-3 py-1 cursor-pointer`}>XXL</p>
           </div>
 
         </div>
