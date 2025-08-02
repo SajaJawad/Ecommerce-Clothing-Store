a2f61d2b37951e0904a8bfbeead4446c1296bd9f
SajaJawad <saiamaher2002@gmail.com>
2025-08-02 10:50:08 +0300

    Complete Remove Product

--- admin/src/pages/List.jsx
+++ admin/src/pages/List.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react'
 import { backendUrl, currency } from '../App'
 import { toast } from 'react-toastify'
 
-const List = () => {
+const List = ({ token }) => {
   const [list, setList] = useState([])
 
   const fetchList = async () => {
@@ -18,11 +18,27 @@ const List = () => {
     } catch (error) {
       console.log(error);
       toast.error(error.message)
-      
+
     }
   }
 
-  
+  const removeProduct = async (id) => {
+    try {
+
+      const response = await axios.post(backendUrl + '/api/product/remove', { id }, { headers: { token } })
+
+      if (response.data.success) {
+        toast.success(response.data.message)
+        await fetchList()
+      } else {
+        toast.error(response.data.message)
+      }
+
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message)
+    }
+  }
 
   useEffect(() => {
     fetchList()
@@ -33,30 +49,30 @@ const List = () => {
   return (
 
     <>
-    <p className='mb-2'>All Products List</p>
-    <div className='flex flex-col gap-2'>
-      {/* ----------List Table Tittle--------------- */}
-      <div className='hidden md:grid grid-cols-[1fr_3fr_1fr_1fr_1fr] items-center py-1 px-2 border bg-gray-100 text-sm'>
-       <p>Image</p>
-       <p>Name</p>
-       <p>Category</p>
-       <p>Price</p>
-       <p className='text-center'>Action</p>
-      </div>
+      <p className='mb-2'>All Products List</p>
+      <div className='flex flex-col gap-2'>
+        {/* ----------List Table Tittle--------------- */}
+        <div className='hidden md:grid grid-cols-[1fr_3fr_1fr_1fr_1fr] items-center py-1 px-2 border bg-gray-100 text-sm'>
+          <p>Image</p>
+          <p>Name</p>
+          <p>Category</p>
+          <p>Price</p>
+          <p className='text-center'>Action</p>
+        </div>
 
-      {/*---------Product List---------- */}
-      {
-        list.map((item , index)=>(
-          <div className='grid grid-cols-[1fr_3fr_1fr_1fr_1fr] items-center gap-2 py-1 px-2 border text-sm' key={index}>
-            <img className='w-12' src={item.image[0]} alt="" />
-            <p>{item.name}</p>
-            <p>{item.category}</p>
-            <p>{currency}{item.price}</p>
-            <p className='text-right md:text-center cursor-pointer text-lg'>X</p>
-          </div>
-        ))
-      }
-    </div>
+        {/*---------Product List---------- */}
+        {
+          list.map((item, index) => (
+            <div className='grid grid-cols-[1fr_3fr_1fr_1fr_1fr] items-center gap-2 py-1 px-2 border text-sm' key={index}>
+              <img className='w-12' src={item.image[0]} alt="" />
+              <p>{item.name}</p>
+              <p>{item.category}</p>
+              <p>{currency}{item.price}</p>
+              <p onClick={()=>removeProduct(item._id)} className='text-right md:text-center cursor-pointer text-lg'>X</p>
+            </div>
+          ))
+        }
+      </div>
     </>
   )
 }
