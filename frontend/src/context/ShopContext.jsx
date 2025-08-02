45301bddf6490ca2c8b1cbb8b55f535550a0f9cc
SajaJawad <saiamaher2002@gmail.com>
2025-08-02 11:53:31 +0300

    List Product in Frontend

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,16 +1,18 @@
 import { createContext, useEffect, useState } from "react";
-import { products } from "../assets/assets";
 import { toast } from "react-toastify";
-import {useNavigate} from "react-router-dom"
+import { useNavigate } from "react-router-dom"
+import axios from 'axios'
 
 export const ShopContext = createContext();
 
 const ShopContextProvider = (props) => {
   const currency = "$";
   const delivery_fee = 10;
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
   const [search, setSearch] = useState("");
   const [showSearch, setShowSearch] = useState(false);
   const [cartItems, setCartItems] = useState({});
+  const [products, setProducts] = useState([]);
   const navigate = useNavigate();
 
   const addToCart = async (itemId, size) => {
@@ -41,7 +43,7 @@ const ShopContextProvider = (props) => {
           if (cartItems[items][item] > 0) {
             totalCount += cartItems[items][item];
           }
-        } catch (error) {}
+        } catch (error) { }
       }
     }
 
@@ -55,24 +57,45 @@ const ShopContextProvider = (props) => {
     setCartItems(cartData);
   };
 
-const getCartAmount=() =>{
-  let totalAmount = 0;
-  for(const items in cartItems){
-    let itemInfo = products.find((product)=>product._id===items)
-    for(const item in cartItems[items]){
-      try {
-        if( cartItems[items][item]>0){
-          totalAmount += itemInfo.price * cartItems[items][item]
+  const getCartAmount = () => {
+    let totalAmount = 0;
+    for (const items in cartItems) {
+      let itemInfo = products.find((product) => product._id === items)
+      for (const item in cartItems[items]) {
+        try {
+          if (cartItems[items][item] > 0) {
+            totalAmount += itemInfo.price * cartItems[items][item]
+
+          }
+        } catch (error) {
 
         }
-      } catch (error) {
-        
       }
+    }
+
+    return totalAmount;
+  }
+
+
+  const getProductsData = async () => {
+    try {
+
+      const response = await axios.get(backendUrl + '/api/product/list')
+      if (response.data.success) {
+        setProducts(response.data.products)
+      } else {
+        toast.error(response.data.message)
+      }
+
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message)
     }
   }
 
- return totalAmount ;
-}
+  useEffect(() => {
+    getProductsData()
+  }, [])
 
 
   const value = {
@@ -87,7 +110,7 @@ const getCartAmount=() =>{
     addToCart,
     getCartCount,
     updateQuantity,
-    getCartAmount , navigate
+    getCartAmount, navigate, backendUrl
   };
 
   return (
